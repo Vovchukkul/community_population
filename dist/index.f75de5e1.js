a6c17e5aca048c4b7ddf5064baea9bacb4fee682
Vovchukkul <140501225+Vovchukkul@users.noreply.github.com>
2024-10-09 11:42:36 +0300

    add

--- dist/index.f75de5e1.js
+++
@@ -1,9 +0,0 @@
-const switchSpans = document.querySelectorAll(".top-bar_switch span");
-switchSpans.forEach((span)=>{
-    span.addEventListener("click", ()=>{
-        switchSpans.forEach((s)=>s.classList.remove("top-bar_active"));
-        span.classList.add("top-bar_active");
-    });
-});
-
-//# sourceMappingURL=index.f75de5e1.js.map
