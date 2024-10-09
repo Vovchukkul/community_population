27522e5fc2f79872d0675aebd13349e61b5008f3
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-10-09 21:36:19 +0300

    add demo

--- dist/index.5cecb784.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelectorAll(".top-bar_switch span");t.forEach(a=>{a.addEventListener("click",()=>{t.forEach(t=>t.classList.remove("top-bar_active")),a.classList.add("top-bar_active")})});
-//# sourceMappingURL=index.5cecb784.js.map
