4de583774898a3e8964d823547b338e2698d0347
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-10-13 08:50:36 +0300

    fixed 768px

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 const switchSpans = document.querySelectorAll('.header_switch li');
 
-  switchSpans.forEach(li => {
-    li.addEventListener('click', () => {
-      switchSpans.forEach(s => s.classList.remove('header_active'));
-      li.classList.add('header_active');
-    });
+switchSpans.forEach(li => {
+  li.addEventListener('click', () => {
+    switchSpans.forEach(s => s.classList.remove('header_active'));
+    li.classList.add('header_active');
   });
+});
 
 // let width = window.innerWidth;
 // alert("Width: " + width)
