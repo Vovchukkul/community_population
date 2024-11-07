fc71cd5e67be436eb4bb622ff6bae7be7dc66392
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2024-11-07 18:21:52 +0200

    add fixed bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,33 @@ paths.forEach(a => {
 
 // let width = window.innerWidth;
 // alert("Width: " + width)
+
+$(function () {
+  // Initialize ScrollMagic controller
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave',
+      duration: "200%"
+    }
+  });
+
+  // Select the elements that need the ScrollMagic behavior
+  var header = document.querySelector("header");
+  var firstSection = document.querySelector(".main");
+
+  // Create ScrollMagic scene for the header
+  new ScrollMagic.Scene({
+      triggerElement: header
+    })
+    .setPin(header, { pushFollowers: false })
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  // Create ScrollMagic scene for the first section
+  new ScrollMagic.Scene({
+      triggerElement: firstSection
+    })
+    .setPin(firstSection, { pushFollowers: false })
+    // .addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
